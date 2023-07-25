4dc6624b041be5d2c0baa65b0be2b5b337058e76
WanjikuN <cikunjoroge4@gmail.com>
2023-07-25 19:11:39 +0300

    Add POST method and catch for error handling

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Add your code here
+let body = document.querySelector('body');
+const submitData = () => {
+    
+    let newObj = {
+      name: "Steve",
+      email: "steve@steve.com"
+    };
+    fetch("http://localhost:3000/users", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+          },
+      body: JSON.stringify(newObj)
+    })
+    .then(resp => resp.json())
+    .then(d => {
+        
+        let p = document.createElement('p')
+        p.textContent = d.id;
+        body.appendChild(p)
+        // listy is an error
+    listy()
+    } 
+        )
+    .catch(err => {
+        let p1 = document.createElement('p')
+        let msg = "Unauthorized Access"
+        p1.textContent = msg
+        body.appendChild(p1)
+        
+            // document.body.innerHTML += err.msg
+         
+    })
+  };
+  
+  submitData();
